cb27789e696d2ebcc28e9f9f7121cc1916a998ae
billmath <billmath@microsoft.com>
2018-08-17 10:01:12 -0400

    updating

---
+++ 3_InstallAD.ps1
@@ -0,0 +1,36 @@
+# Filename:    3_InstallAD.ps1
+# Description: Creates an on-premises AD envrionment.  This is part of
+#              the Azure AD Connect password hash sync tutorial.
+#
+# DISCLAIMER:
+# Copyright (c) Microsoft Corporation. All rights reserved. This 
+# script is made available to you without any express, implied or 
+# statutory warranty, not even the implied warranty of 
+# merchantability or fitness for a particular purpose, or the 
+# warranty of title or non-infringement. The entire risk of the 
+# use or the results from the use of this script remains with you.
+#
+#
+#
+#
+#Declare variables
+$DatabasePath = "c:\windows\NTDS"
+$DomainMode = "WinThreshold"
+$DomainName = "contoso.com"
+$DomaninNetBIOSName = "CONTOSO"
+$ForestMode = "WinThreshold"
+$LogPath = "c:\windows\NTDS"
+$SysVolPath = "c:\windows\SYSVOL"
+$featureLogPath = "c:\poshlog\featurelog.txt" 
+$Password = "Pass1w0rd"
+
+#Install AD DS, DNS and GPMC 
+start-job -Name addFeature -ScriptBlock { 
+Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools } 
+Wait-Job -Name addFeature 
+Get-WindowsFeature | Where installed >>$featureLogPath
+
+#Create New AD Forest
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $DatabasePath -DomainMode $DomainMode -DomainName $DomainName -SafeModeAdministratorPassword $Password -DomainNetbiosName $DomainNetBIOSName -ForestMode $ForestMode -InstallDns:$true -LogPath $LogPath -NoRebootOnCompletion:$false -SysvolPath $SysVolPath -Force:$true
---
+++ 4_CreateUser.ps1
@@ -0,0 +1,30 @@
+﻿# Filename:    4_CreateUser.ps1
+# Description: Creates a user in Active Directory.  This is part of
+#              the Azure AD Connect password hash sync tutorial.
+#
+# DISCLAIMER:
+# Copyright (c) Microsoft Corporation. All rights reserved. This 
+# script is made available to you without any express, implied or 
+# statutory warranty, not even the implied warranty of 
+# merchantability or fitness for a particular purpose, or the 
+# warranty of title or non-infringement. The entire risk of the 
+# use or the results from the use of this script remains with you.
+#
+#
+#
+#
+#Declare variables
+$Givenname = "Allie"
+$Surname = "McCray"
+$Displayname = "Allie McCray"
+$Name = "amccray"
+$Password = "Pass1w0rd"
+$Identity = "CN=ammccray,CN=Users,DC=contoso,DC=com"
+$SecureString = ConvertTo-SecureString $Password -AsPlainText -Force
+
+
+#Create the user
+New-ADUser -Name $Name -GivenName $Givenname -Surname $Surname -DisplayName $Displayname -AccountPassword $SecureString
+
+#Set the password to never expire
+Set-ADUser -Identity $Identity -PasswordNeverExpires $true -ChangePasswordAtLogon $false -Enabled $true
