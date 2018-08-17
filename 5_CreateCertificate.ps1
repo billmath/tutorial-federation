a3594db40404716cf9d04280998168f6ed1696e7
billmath <billmath@microsoft.com>
2018-08-17 10:09:10 -0400

    updating

---
+++ 5_CreateCertificate.ps1
@@ -0,0 +1,21 @@
+﻿# Filename:    5_CreateCertificate.ps1
+# Description: Creates a certificate to be used by AD FS.  This is part of
+#              the Azure AD Connect password hash sync tutorial.
+#
+# DISCLAIMER:
+# Copyright (c) Microsoft Corporation. All rights reserved. This 
+# script is made available to you without any express, implied or 
+# statutory warranty, not even the implied warranty of 
+# merchantability or fitness for a particular purpose, or the 
+# warranty of title or non-infringement. The entire risk of the 
+# use or the results from the use of this script remains with you.
+#
+#
+#
+#
+#Declare variables
+$DNSname = "adfs.contoso.com"
+$Location = "cert:\LocalMachine\My"
+
+#Create certificate
+New-SelfSignedCertificate -DnsName $DNSname -CertStoreLocation $Location
